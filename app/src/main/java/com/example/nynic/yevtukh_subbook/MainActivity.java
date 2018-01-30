5ea3a7e1b78ca7cb19b4b2ee2962f1efbcb6ad03
xwafflecakex <nyevtukh@gmail.com>
2018-01-29 16:43:07 -0800

    Uploading project to Git.

---
+++ app/src/main/java/com/example/nynic/yevtukh_subbook/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nynic.yevtukh_subbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
